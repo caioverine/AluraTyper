284c7e31e0416e05f1dbacfb4d8ede5e6872c1d3
caio.cvac <caio.cvac@dpf.gov.br>
2017-12-22 11:55:11 -0200

    Aula 04

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,52 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
 var campo = $(".campo-digitacao");
-var tempoRestante = $("#tempo-digitacao").text();
+var tempoInicial = $("#tempo-digitacao").text();
 
-tamanhoFrase.text(numPalavras);
-
-campo.on("input", function() {
-
-  var conteudo = campo.val();
-  var qtdPalavras = conteudo.split(/\S+/).length -1;
-  var qtdCaracteres = conteudo.length;
-
-  $("#contador-palavras").text(qtdPalavras);
-  $("#contador-caracteres").text(qtdCaracteres);
+$(function(){
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-campo.one("focus", function() {
+function atualizaTamanhoFrase() {
 
-  var cronometroID = setInterval(function() {
-    tempoRestante--;
-    $("#tempo-digitacao").text(tempoRestante);
-    if (tempoRestante < 1) {
-      campo.attr("disabled", true);
-      clearInterval(cronometroID);
-    }
-  }, 1000);
-});
+  var frase = $(".frase").text();
+  var numPalavras  = frase.split(" ").length;
+  var tamanhoFrase = $("#tamanho-frase");
+  tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+  campo.on("input",function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+  });
+}
+
+function inicializaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus", function() {
+    var cronometroID = setInterval(function() {
+      tempoRestante--;
+      $("#tempo-digitacao").text(tempoRestante);
+      if (tempoRestante < 1) {
+        campo.attr("disabled", true);
+        clearInterval(cronometroID);
+      }
+    }, 1000);
+  });
+}
+
+function reiniciaJogo(){
+  campo.attr("disabled",false);
+  campo.val("");
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  $("#tempo-digitacao").text(tempoInicial);
+  inicializaCronometro();
+}
